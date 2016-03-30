9642eb814a050c592f57c187ab809abdbb8971dc
galefeilian <137113610@qq.com>
2016-03-30 12:50:48 -0400

    update lose massage

--- TTTController.js
+++ TTTController.js
@@ -29,7 +29,7 @@ function addEventListenerForCells(tableCell){
 				gameModel.makeMove(play[0],play[1])
 				tableCell.parentNode.parentNode.rows[play[0]].cells[play[1]].innerHTML = gameModel.gameBoard[play[0]][play[1]];
 				if(gameModel.playerWin()){
-					alert("Loser")
+					alert("YOU ARE A LOSER")
 				}else if (gameModel.isDraw()){
 					alert("Draw")
 				}
